a16e60fdf70956cdb65933f809e511306e4f3724
joshccng <ng_josh96@hotmail.com>
2022-08-31 22:05:18 -0700

    Close dropdown when clicking outside of it

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
-function navbarCollapse() {
-    var x = document.getElementById("websiteNavbar");
+const navbarId = "websiteNavbar";
+const navbarEl = document.getElementById(navbarId);
+const navbarClass = "website-navbar";
+const hamburgerClass = "fa-bars";
+const navbarDropdownClass = "navbar-collapse-content";
 
-    if (x.className === "website-navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "website-navbar";
+document.addEventListener("click", (e) => {
+    const isHamburgerClick = e.target.className.includes(hamburgerClass);
+
+    var elem = e.target;
+    var isDropdownClick = false;
+
+    // Check whether anything within the dropdown is clicked
+    for (; elem && elem !== document; elem = elem.parentNode) {
+        if (elem.className.includes(navbarDropdownClass)) {
+            isDropdownClick = true;
+            break;
+        }
     }
-}
+
+    if (isHamburgerClick) {
+        if (navbarEl.className === navbarClass) {
+            navbarEl.className += " responsive";
+        } else {
+            navbarEl.className = navbarClass;
+        }
+    } else if (!isDropdownClick && navbarEl.className.includes("responsive")) {
+        // The dropdown is open and the click came from outside the dropdown
+        navbarEl.className = navbarClass;
+    }
+})
