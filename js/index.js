3893806501e1f21f3af9000ee0ab610481bd0e39
joshccng <ng_josh96@hotmail.com>
2022-09-07 21:39:36 -0700

    Open and close responsive work dropdown by clicking

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const navbarId = "websiteNavbar";
 const navbarEl = document.getElementById(navbarId);
 const navbarClass = "website-navbar";
+const workDropdownId = "dropdownContent";
+const workDropdownEl = document.getElementById(workDropdownId);
+const workDropdownClass = "navbar-dropdown-content";
 const hamburgerClass = "fa-bars";
 const navbarDropdownClass = "navbar-collapse-content";
 
@@ -22,10 +25,24 @@ document.addEventListener("click", (e) => {
         if (navbarEl.className === navbarClass) {
             navbarEl.className += " responsive";
         } else {
+            // Close all dropdowns if the dropdown is open and the hamburger is clicked.
             navbarEl.className = navbarClass;
+            workDropdownEl.className = workDropdownClass;
         }
     } else if (!isDropdownClick && navbarEl.className.includes("responsive")) {
-        // The dropdown is open and the click came from outside the dropdown
+        // The dropdown is open and the click came from outside the dropdown,
+        // so close the dropdown. Also, reset (close) the work dropdown.
         navbarEl.className = navbarClass;
+        workDropdownEl.className = workDropdownClass;
     }
 })
+
+function workDropdownToggle() {
+    if (navbarEl.className.includes("responsive")) {
+        if (workDropdownEl.className === workDropdownClass) {
+            workDropdownEl.className += " open";
+        } else {
+            workDropdownEl.className = workDropdownClass;
+        }
+    }
+}
